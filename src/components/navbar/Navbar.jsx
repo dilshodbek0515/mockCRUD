5db62f11ed54a79b71814fca1acbc9ff3117cefc
dilshod <dilshodx516@gmail.com>
2024-11-22 18:00:32 +0500

    crud

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -6,10 +6,7 @@ function Navbar() {
   const users = useSelector(s => s.users.value)
   return (
     <div className='navbar'>
-      <h2>Redux Toolkit</h2>
-      <p>Home</p>
-      <NavLink to={"/create-user"}>Create user</NavLink>
-      <p>All users</p>
+      <NavLink className="cards" to={"/create-user"}>Cards</NavLink>
     </div>
   )
 }
