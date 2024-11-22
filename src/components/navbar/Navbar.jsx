cc95fad31b97149916fee04a91dde5135c87bc65
dilshod <dilshodx516@gmail.com>
2024-11-22 18:53:28 +0500

    crud

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,12 +1,11 @@
 import React from 'react'
 import "./Navbar.css"
 import { NavLink } from 'react-router-dom'
-import { useSelector } from 'react-redux'
 function Navbar() {
-  const users = useSelector(s => s.users.value)
   return (
     <div className='navbar'>
-      <NavLink className="cards" to={"/create-user"}>Cards</NavLink>
+      <NavLink className="cards" to={"/"}>Cards</NavLink>
+      <NavLink className="" to={"/create-user"}>Create User</NavLink>
     </div>
   )
 }
